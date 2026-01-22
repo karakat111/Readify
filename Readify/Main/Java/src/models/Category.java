b22fea8ef1a3497060c3a168320d596960e2b9c8
karakat111 <253442@astanait.edu.kz>
2026-01-22 15:42:55 +0000

    Update Category.java

--- Readify/Main/Java/src/models/Category.java
+++ Readify/Main/Java/src/models/Category.java
@@ -1,25 +1,11 @@
-package src.models;
+package models;
 
-import jakarta.persistence.*;
-import java.util.List;
-
-@Entity
-@Table(name = "categories")
 public class Category {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
     private String name;
 
-    @OneToMany(mappedBy = "category")
-    private List<Book> books;
-
+    public Category(Long id, String name) { this.id = id; this.name = name; }
     public Long getId() { return id; }
-
     public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
 }
 
