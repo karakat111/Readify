7959da81bd913102d020be41a392c4e7a75c5c20
karakat111 <253442@astanait.edu.kz>
2026-01-22 15:44:51 +0000

    Update Reservation.java

--- Readify/Main/Java/src/models/Reservation.java
+++ Readify/Main/Java/src/models/Reservation.java
@@ -1,38 +1,14 @@
-package src.models;
+package models;
 
-import jakarta.persistence.*;
-import java.time.LocalDate;
-
-@Entity
-@Table(name = "reservations")
 public class Reservation {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
-    private LocalDate reservationDate;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
     private User user;
-
-    @ManyToOne
-    @JoinColumn(name = "book_id", nullable = false)
     private Book book;
 
-    public Long getId() { return id; }
-
-    public LocalDate getReservationDate() { return reservationDate; }
-    public void setReservationDate(LocalDate reservationDate) {
-        this.reservationDate = reservationDate;
+    public Reservation(Long id, User user, Book book) {
+        this.id = id; this.user = user; this.book = book;
     }
 
     public User getUser() { return user; }
-    public void setUser(User user) { this.user = user; }
-
     public Book getBook() { return book; }
-    public void setBook(Book book) { this.book = book; }
 }
-
